2f68406b4b42d362bfbb3c5b573535e2680a5c68
Gerben <gerben.g@hotmail.com>
2017-07-21 11:25:30 +0200

    CheeseDAOInterface update

--- KaasApplicatie/src/main/java/Interface/CheeseDAOInterface.java
+++ KaasApplicatie/src/main/java/Interface/CheeseDAOInterface.java
@@ -6,6 +6,7 @@
 package Interface;
 
 import POJO.CheesePOJO;
+import java.math.BigDecimal;
 import java.util.List;
 
 
@@ -21,8 +22,16 @@ public interface CheeseDAOInterface {
     
     public CheesePOJO getCheese(CheesePOJO cheese);
     
+    public CheesePOJO getCheese(String cheesename);
+    
     public void updateCheese(CheesePOJO cheese);
     
-    public boolean deleteCheese (CheesePOJO cheese);
+    public void updateCheese(Integer CheeseID, String newName);
+    
+    public void updateCheese(Integer CheeseID, BigDecimal price);
+    
+    public void updateCheese(Integer CheeseID, Integer stock);
+    
+    public void deleteCheese (CheesePOJO cheese);
     
 }
